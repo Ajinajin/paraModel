fa4c048553e25f0539ce1a5c0167bf14e6bed8d1
dongbin6 <405581852@qq.com>
2021-12-15 21:43:35 +0800

    改

--- ParaModel/main.cpp
+++ ParaModel/main.cpp
@@ -1,6 +1,6 @@
 
-//#include "WarheadParaModel.h"
-#include "ParaModel.h"
+#include "WarheadParaModel.h"
+//#include "ParaModel.h"
 #include <QtWidgets/QApplication>
 #include <QApplication>
 #include <QDesktopWidget>
@@ -12,8 +12,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-	//WarheadParaModel w;
-	ParaModel w;
+	WarheadParaModel w;
+	//ParaModel w;
 	w.show();
 	//计时开始
 	LARGE_INTEGER litmp;
